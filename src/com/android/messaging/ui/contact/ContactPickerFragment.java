LineageOS/android_packages_apps_Messaging

Messaging: Fix warnings about drawables and colors
* They need the theme provided now
* Follow the suggestion by AS
  (sometimes we use ResourcesCompat, sometimes not)

Change-Id: Ie3023fee13a6fc04bd4b912721ba19dcc08befba

src/com/android/messaging/ui/contact/ContactPickerFragment.java
@@ -552,8 +552,9 @@ public class ContactPickerFragment extends Fragment implements ContactPickerData
         // Hide the action bar for contact picker mode. The custom ToolBar containing chips UI
         // etc. will take the spot of the action bar.
         actionBar.hide();
-        UiUtils.setStatusBarColor(getActivity(),
-                getResources().getColor(R.color.compose_notification_bar_background));
+        UiUtils.setStatusBarColor(requireActivity(),
+                getResources().getColor(R.color.compose_notification_bar_background,
+                        requireActivity().getTheme()));
     }
 
     private GetOrCreateConversationActionMonitor mMonitor;

DroidX-UI/packages_apps_Messaging

Signed-off-by: NRanjan-17 <nalinishranjan05@gmail.com>

src/com/android/messaging/ui/contact/ContactPickerFragment.java
@@ -174,7 +174,7 @@ public class ContactPickerFragment extends Fragment implements ContactPickerData
         mCustomHeaderViewPager.setViewHolders(viewHolders);
         mCustomHeaderViewPager.setViewPagerTabHeight(CustomHeaderViewPager.DEFAULT_TAB_STRIP_SIZE);
         mCustomHeaderViewPager.setBackgroundColor(getResources()
-                .getColor(R.color.contact_picker_background));
+                .getColor(R.color.contact_picker_background, getContext().getTheme()));
 
         // The view pager defaults to the frequent contacts page.
         mCustomHeaderViewPager.setCurrentItem(0);
@@ -554,7 +554,8 @@ public class ContactPickerFragment extends Fragment implements ContactPickerData
         // etc. will take the spot of the action bar.
         actionBar.hide();
         UiUtils.setStatusBarColor(getActivity(),
-                getResources().getColor(R.color.compose_notification_bar_background));
+                getResources().getColor(R.color.compose_notification_bar_background,
+                        getActivity().getTheme()));
     }
 
     private GetOrCreateConversationActionMonitor mMonitor;